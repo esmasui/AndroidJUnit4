6e1a54421d6e31b8331542a0840797524efb9460
Sosuke Masui <esmasui@gmail.com>
2012-09-15 04:51:54 +0900

    Correct typo.

--- android-junit4-extension/src/main/java/com/uphyca/testing/extension/SQLiteOpenHelperTestCase.java
+++ android-junit4-extension/src/main/java/com/uphyca/testing/extension/SQLiteOpenHelperTestCase.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.uphyca.testing.junit4.extnsion;
+package com.uphyca.testing.extension;
 
 import org.junit.After;
 import org.junit.Before;
--- android-junit4-extension/src/main/java/com/uphyca/testing/extension/SQLiteOpenHelperTester.java
+++ android-junit4-extension/src/main/java/com/uphyca/testing/extension/SQLiteOpenHelperTester.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.uphyca.testing.junit4.extnsion;
+package com.uphyca.testing.extension;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteOpenHelper;
